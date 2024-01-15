fedd19f0a83a962252904cec3c49caab1a7d1124
Aren Hovsepyan <ar.hovsepyan2011@gmail.com>
2024-01-15 12:53:35 +0400

    feat: add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,17 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  integrations: [tailwind(), react(), mdx()],
+  integrations: [
+    tailwind(),
+    react(),
+    mdx(),
+    node({
+      mode: "standalone",
+    }),
+  ],
 });
